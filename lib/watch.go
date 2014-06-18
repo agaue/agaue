54798a888e6b241affcba292b6ee366709598946
Damon Zhao <yijun.zhao@outlook.com>
2014-06-18 17:31:25 +0800

    finish watch.go

--- lib/watch.go
+++ lib/watch.go
@@ -19,4 +19,43 @@ func launchWatcher() *fsnotify.Watcher {
 		log.Fatal("FATAL", err)
 	}
 	go watch(w)
+	// watch posts directory
+	if err = w.Watch(PostsDir); err != nil {
+		w.Close()
+		log.Fatal("FATAL", err)
+	}
+	// watch templates directory
+	if err = w.Watch(TemplatesDir); err != nil {
+		w.Close()
+		log.Fatal("FATAL", err)
+	}
+	return w
+}
+
+// lazy
+func watch(w *fsnotify.Watcher) {
+	var delay <-chan time.Time
+	for {
+		select {
+
+		case ev := <-w.Event:
+			ext := filepath.Ext(ev.Name)
+			if strings.HasPrefix(ev.Name, PostsDir) && ext == ".md" {
+				delay = time.After(eventDelay)
+			} else if strings.HasPrefix(ev.Name, TemplatesDir) && ext == ".html" {
+				delay = time.After(eventDelay)
+			}
+
+		case err := <-w.Error:
+			log.Println("Watch Error: ", err)
+
+		case <-delay:
+			if err := GenerateSite(); err != nil {
+				log.Println("Error generating site: ", err)
+			} else {
+				log.Println("site generated")
+			}
+
+		}
+	}
 }
